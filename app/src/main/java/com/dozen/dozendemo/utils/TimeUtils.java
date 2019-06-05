efdacf3780e68e01c785c656e7e2f95655808a28
hugo <hugo@example.com>
2019-06-05 14:19:02 +0800

    加入Utils,实用工具

---
+++ app/src/main/java/com/dozen/dozendemo/utils/TimeUtils.java
@@ -0,0 +1,30 @@
+package com.dozen.dozendemo.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+
+public class TimeUtils {
+
+
+    public static boolean areSameDay(Date dateA, Date dateB) {
+        Calendar calDateA = Calendar.getInstance();
+        calDateA.setTime(dateA);
+
+        Calendar calDateB = Calendar.getInstance();
+        calDateB.setTime(dateB);
+
+        return calDateA.get(Calendar.YEAR) == calDateB.get(Calendar.YEAR) &&
+                calDateA.get(Calendar.MONTH) == calDateB.get(Calendar.MONTH) &&
+                calDateA.get(Calendar.DAY_OF_MONTH) ==
+                        calDateB.get(Calendar.DAY_OF_MONTH);
+    }
+
+
+    public static String getSystemDate() {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss");
+        return simpleDateFormat.format(date);
+    }
+}
